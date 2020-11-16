308b2d9394f1fe6ccfa3dd896ea55bcd9da4140f
neerajpremani <neerajpremani@gmail.com>
2020-11-16 16:30:48 -0500

    Spring Boot Initialize

---
+++ src/main/java/com/neeraj/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+}
